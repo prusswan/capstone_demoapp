dded45ec227c20f3a4db0c4333fd1f95b01f40cf
prusswan <prusswan@gmail.com>
2017-02-23 18:02:38 +0800

    updated faker to generate role positions

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -4,6 +4,11 @@ FactoryGirl.define do
     user_id { create(:dummy_user).id }
     role_name "organizer"
     mname "Role"
+
+    trait :random do
+      role_name { Faker::Job.position.parameterize }
+    end
+
     after(:create) do |role|
       if role.mid.nil?
         role.update(mid: role.id)

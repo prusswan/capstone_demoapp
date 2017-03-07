80ce8d6823648add1fb7652857ac103a26f8b927
prusswan <prusswan@gmail.com>
2017-03-07 20:21:55 +0800

    add image_id to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170130051702) do
+ActiveRecord::Schema.define(version: 20170307100345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20170130051702) do
     t.json     "tokens"
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
+    t.integer  "image_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree

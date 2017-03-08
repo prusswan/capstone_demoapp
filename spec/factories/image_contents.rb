26758a7d6fa4dc7d95a25c70e8a0e26430959884
prusswan <prusswan@gmail.com>
2017-03-08 14:53:55 +0800

    added factory for user avatars

--- spec/factories/image_contents.rb
+++ spec/factories/image_contents.rb
@@ -4,11 +4,25 @@ FactoryGirl.define do
     content_type "image/jpg"
         #default to the original size when we generate just attributes
     sequence(:content) {|idx|
-      File.open(ImageContentHelper.sample_filepath,"rb") { |f| 
+      File.open(ImageContentHelper.sample_filepath,"rb") { |f|
         image=StringIO.new(f.read)
         image=ImageContentCreator.annotate(idx, image)
         Base64.encode64(image)
       }
     }
+
+    trait :random_avatar do
+      content do
+        color = Faker::Color.hsl_color
+
+        MiniMagick::Tool::Convert.new do |c|
+          c.size "30x20"
+          c.gradient "hsl(#{color[0]}, #{color[1]*100}%, #{color[2]*100}%)"
+          c << "tmp/avatar.jpg"
+        end
+        image = File.open('tmp/avatar.jpg').read
+        Base64.encode64(image)
+      end
+    end
   end
 end

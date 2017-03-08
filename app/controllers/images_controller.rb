7752abdc05c2c149a2729a7ce5da657a2df7cd3a
prusswan <prusswan@gmail.com>
2017-03-09 03:00:59 +0800

    added scope for Image.without_users

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -9,7 +9,7 @@ class ImagesController < ApplicationController
 
   def index
     authorize Image
-    @images = policy_scope(Image.all)
+    @images = policy_scope(Image.without_users)
     @images = ImagePolicy.merge(@images)
   end
 
@@ -22,7 +22,7 @@ class ImagesController < ApplicationController
   def content
     result=ImageContent.image(@image).smallest(params[:width],params[:height]).first
     if result
-      expires_in 1.year, :public=>true 
+      expires_in 1.year, :public=>true
       if stale? result
         options = { type: result.content_type,
                     disposition: "inline",
@@ -43,7 +43,7 @@ class ImagesController < ApplicationController
       if @image.save
         original=ImageContent.new(image_content_params)
         contents=ImageContentCreator.new(@image, original).build_contents
-        if (contents.save!) 
+        if (contents.save!)
           role=current_user.add_role(Role::ORGANIZER, @image)
           @image.user_roles << role.role_name
           role.save!
@@ -96,9 +96,9 @@ class ImagesController < ApplicationController
       Rails.logger.debug exception
     end
 
-    def mongoid_validation_error(exception) 
+    def mongoid_validation_error(exception)
       payload = { errors:exception.record.errors.messages
-                     .slice(:content_type,:content,:full_messages) 
+                     .slice(:content_type,:content,:full_messages)
                      .merge(full_messages:["unable to create image contents"])}
       render :json=>payload, :status=>:unprocessable_entity
       Rails.logger.debug exception.message
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ActiveRecord::Base
 
   has_many :roles, inverse_of: :user, dependent: :destroy
 
+  belongs_to :image
+
+  scope :with_images, -> { where.not(image_id: nil) }
+
   def has_role(role_list, mname=nil, mid=nil)
     role_names=roles.relevant(mname, mid).map {|r| r.role_name}
     (role_names & role_list).any?
